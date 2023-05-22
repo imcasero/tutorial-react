2e99b9a517b713f5b89e11339894d8964e633926
imcasero <diegocaserosmr@gmail.com>
2023-05-22 20:53:29 +0200

    Estructura = componentes tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
+import TaskListComponent from "./components/container/task_list";
 // eslint-disable-next-line no-unused-vars
 import Greeting from "./components/pure/greeting";
+// eslint-disable-next-line no-unused-vars
 import GreetingF from "./components/pure/greetingF";
 
 function App() {
@@ -12,16 +14,10 @@ function App() {
       <div>
         {/* Componente propio */}
         {/* <Greeting name="Diego"></Greeting> */}
-        <GreetingF name="Diego"></GreetingF>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <GreetingF name="Diego"></GreetingF> */}
+        {/* Componente de listado de tareas */}
+        <TaskListComponent></TaskListComponent>
       </div>
-      <h1>Vite + React</h1>
-      <h2>Hola react!</h2>
     </>
   );
 }
